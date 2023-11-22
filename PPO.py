5e36c9cf5285fdf433ab7f8d2c4494ef346580a3
Simon Fikse <thearchseal@gmail.com>
2023-11-22 14:29:01 +0100

    comments

--- PPO.py
+++ PPO.py
@@ -1,19 +1,23 @@
+# import game and action space
 import gym_super_mario_bros
 from nes_py.wrappers import JoypadSpace
 from gym_super_mario_bros.actions import SIMPLE_MOVEMENT
 
+# import preprocessing tools
 from gym.wrappers import GrayScaleObservation
 from stable_baselines3.common.vec_env import VecFrameStack, DummyVecEnv
 
+# import AI model and tools to automatically save it
 import os
 from stable_baselines3 import PPO
 from stable_baselines3.common.callbacks import BaseCallback
 
-
+# file paths for models and logs
 CHECKPOINT_DIR = ".\\models\\"
 LOG_DIR = ".\\logs\\"
 
 
+# callback class to automatically save models
 class TrainAndLoggingCallback(BaseCallback):
     def __init__(self, check_freq, save_path, verbose=1):
         super().__init__(verbose)
@@ -31,17 +35,20 @@ class TrainAndLoggingCallback(BaseCallback):
         return True
 
 
+# resolves compatibility issues caused by version mismatches
 JoypadSpace.reset = lambda self, **kwargs: self.env.reset(**kwargs)
+# setup environment
 env = gym_super_mario_bros.make(
     "SuperMarioBros-v0", apply_api_compatibility=True, render_mode="human"
 )
 env = JoypadSpace(env, SIMPLE_MOVEMENT)
+# preprocess frame data
 env = GrayScaleObservation(env, keep_dim=True)
 env = DummyVecEnv([lambda: env])
 env = VecFrameStack(env, 4, channels_order="last")
 
+# setup model and auto saves
 callback = TrainAndLoggingCallback(check_freq=100000, save_path=CHECKPOINT_DIR)
-
 model = PPO(
     "CnnPolicy",
     env,
@@ -51,4 +58,5 @@ model = PPO(
     n_steps=512,
 )
 
+# run the model
 model.learn(total_timesteps=100000, callback=callback)
