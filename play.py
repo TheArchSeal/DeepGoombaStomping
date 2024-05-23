269cd97b84fa9015d52c9305bee9aa5c2ec6ff24
Simon Fikse <thearchseal@gmail.com>
2024-05-23 13:02:02 +0200

    commented play.py

--- play.py
+++ play.py
@@ -1,13 +1,16 @@
+# import dependencies
 import gym_super_mario_bros as smb
 from nes_py.wrappers import JoypadSpace
 import pygame
 
 
+# returns the powerset, excluding [], from a list
 def powerset(s):
     for i in range(1, 1 << len(s)):
         yield [e for j, e in enumerate(s) if i >> j & 1]
 
 
+# define constants
 FPS = 60
 RESOLUTION = (256, 240)
 ACTIONS = ["up", "down", "right", "left", "A", "B"]
@@ -27,18 +30,22 @@ KEYBINDS = {
     pygame.K_z: "B",
 }
 
+# setup environment
 env = smb.make(
     "SuperMarioBros-v0", apply_api_compatibility=True, render_mode="rgb_array"
 )
 env = JoypadSpace(env, ACTION_SPACE)
 
+# setup window
 pygame.init()
 screen = pygame.display.set_mode(RESOLUTION)
 clock = pygame.time.Clock()
 
+# cumulative user score
 total_reward = 0
 
 
+# exit the program
 def stop():
     print("\nYour reward: ", total_reward)
     env.close()
@@ -46,11 +53,13 @@ def stop():
     exit()
 
 
+# game loop
 state = env.reset()
 keys = set()
 done = False
 while not done:
 
+    # input handling
     for event in pygame.event.get():
         match event.type:
             case pygame.QUIT:
@@ -62,10 +71,12 @@ while not done:
                 if event.key in KEYBINDS:
                     keys.remove(KEYBINDS[event.key])
 
+    # take action
     action = sum(1 << ACTIONS.index(key) for key in keys)
     state, reward, done, truncate, info = env.step(action)
     total_reward += reward
 
+    # render to screen
     frame = env.render()
     surface = pygame.pixelcopy.make_surface(frame.transpose(1, 0, 2))
     screen.blit(surface, (0, 0))
